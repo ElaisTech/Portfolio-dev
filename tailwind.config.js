a8f68de2aabd02d7c6d0ac0ea4ba669256a9a7de
ElaisTech <86303399+ElaisTech@users.noreply.github.com>
2022-05-07 14:24:57 +0200

    Design, js, html
    Fixed design and replacing css with tailwindcss.
    Adding javascript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ["./proj/index.html", "./proj/index.js"],
-  theme: {  extend: {},
+  theme: {  extend: {
+    backgroundImage: {
+      'speech-cloud': "url('https://i.ibb.co/VV81q1z/1200px-Speech-balloon-svg.png')",
+      'footer-texture': "url('/img/footer-texture.png')",
+    }
+  },
   },
   plugins: [],
 }
