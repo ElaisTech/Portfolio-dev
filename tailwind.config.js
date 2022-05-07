9689bf4bf42250502f51d7f1769f68d10ea7d74d
ElaisTech <86303399+ElaisTech@users.noreply.github.com>
2022-05-07 09:54:11 +0200

    fixed css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   content: ["./proj/index.html", "./proj/index.js"],
-  theme: {
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-    }
-    extend: {},
+  theme: {  extend: {},
   },
   plugins: [],
 }
